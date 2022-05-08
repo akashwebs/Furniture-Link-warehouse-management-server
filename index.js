e75a6ef3363136f7e5c14b80b076feceac81fdb4
akashwebs <aksah.shil227@gmail.com>
2022-05-08 17:40:43 +0600

    added runing root

--- index.js
+++ index.js
@@ -113,6 +113,9 @@ app.post('/login',async(req,res)=>{
 }
 run().catch(console.dir)
 
+app.get('/',(req,res)=>{
+  res.send('Running Furniture server')
+})
 app.listen(port, ()=>{
     console.log('successfully run server', port)
 })
